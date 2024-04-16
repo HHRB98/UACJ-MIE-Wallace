31bc9973bda1f42451f7694e28872c3f00d5dfeb
HHRB98 <167050019+HHRB98@users.noreply.github.com>
2024-04-15 21:05:11 -0600

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -3,7 +3,7 @@
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
-module tb ();
+module tt_um_wallace_hhrb98 ();
 
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
